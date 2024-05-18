908a62bed9d90a8ba5d917f88069e6a848d98ddd
Mohankrishna1201 <21EE01037@iitbbs.ac.in>
2024-05-18 20:09:35 +0530

    backend deploy3

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require("path")
 const app = express();
 app.use(express.json());
 const corsOptions = {
-    origin: 'http://localhost:5173/', // Remove trailing slash
+    origin: 'https://mohankrishna1201f.onrender.com/', // Remove trailing slash
     credentials: true,
     optionsSuccessStatus: 200,
 };
