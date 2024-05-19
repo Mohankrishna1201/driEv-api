55e765f55bd5ec9fbfd6ea6665c6edb99fec5597
Kattamuri Mohan Krishna <121288107+Mohankrishna1201@users.noreply.github.com>
2024-05-19 08:17:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require("path")
 const app = express();
 app.use(express.json());
 const corsOptions = {
-    origin: true, 
+    origin: '*', 
     methods :["POST,GET,PUT,DELETE"],
     credentials: true,
     optionsSuccessStatus: 200,
